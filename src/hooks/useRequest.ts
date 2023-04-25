a6906758d3b631e4731640d41302d07b8e98f181
hm <605645849@qq.com>
2023-04-25 17:16:47 +0800

    瞎写的

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,8 +1,34 @@
+import { useState, useCallback } from 'react'
+import axios, { AxiosResponse } from 'axios'
 
+interface RequestOptions {
+  url: string;
+  method: string;
+  data?: any;
+}
+
+//useRequest hook helps us to make requests, store the data, error or loading status,
+// that we got in response.
+//It takes a RequestOptions as a parameter and returns an object with data,
+// error, loading and a makeRequest function.
 const useRequest = () => {
+  const [data, setData] = useState<any>(null)
+  const [error, setError] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(false)
 
+  const makeRequest = useCallback(async (options: RequestOptions) => {
+    try {
+      setLoading(true)
+      const response: AxiosResponse = await axios(options)
+      setData(response.data)
+    } catch (err: any) {
+      setError(err.response?.data ?? err.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
 
+  return { data, error, loading, makeRequest }
 }
 
 export default useRequest
-
